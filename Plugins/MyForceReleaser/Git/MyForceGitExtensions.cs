76fd7ad074ef735fa78b489c65071e37d99c0918
Kevin Verfaille <kverfaille@myforce.be>
2016-10-04 14:07:14 +0200

    MyForceGitExtensions: Added cache for git info about the repo. Since this is currently not supposed to change when the plugin is loaded! (Option was added to disable this behavior)

--- Plugins/MyForceReleaser/Git/MyForceGitExtensions.cs
+++ Plugins/MyForceReleaser/Git/MyForceGitExtensions.cs
@@ -9,24 +9,50 @@ namespace MyForceReleaser
     public class MyForceGitExtensions : IMyForceGitModule
     {
         private IGitModule _Git { get; set; }
+        private bool _Cached { get; set; }
+
+        private enum CacheElement { BranchName, WorkingDir, IsMasterBranch, IsFixBranch, IsVersionBranch }
+        private Dictionary<CacheElement, object> _Chache;
+        private delegate object Retriever();
+        private T GetItemFromCache<T>(CacheElement element, Retriever ret)
+        {
+            //Check if we should use the cache
+            if (!_Cached)
+                _Chache.Remove(element);
+
+            //Get the element if its missing
+            if (!_Chache.ContainsKey(element))
+            {
+                _Chache.Add(element, ret());
+                Logger.GetLogger().LogMessage(string.Format("GIT: Get {0} => {1}", element.ToString("f"), (T)_Chache[element]));
+            }
+            return (T)_Chache[element];
+        }
 
         public MyForceGitExtensions(IGitModule GitModule)
         {
             _Git = GitModule;
+            _Chache = new Dictionary<CacheElement, object>();
+            _Cached = true; //As long as the plug in is loaded we don't expect to change branches
+            if (_Cached)
+                Logger.GetLogger().LogMessage("MyForceGitExtensions: Running in cached mode! We don't expect git branch switches during the period the plugin is loaded!");
         }
 
         public string GetCurrentBranchName()
         {
-            string strBranchName = _Git.GetSelectedBranch();
-            Logger.GetLogger().LogMessage(string.Format("GIT: GetCurrentBranchName() => {0}", strBranchName));
-            return strBranchName;
+            return GetItemFromCache<string>(CacheElement.BranchName, 
+                delegate() { 
+                    return _Git.GetSelectedBranch(); 
+                });
         }
 
         public string GetWorkingDir()
         {
-            string strWorkDir = _Git.WorkingDir;
-            Logger.GetLogger().LogMessage(string.Format("GIT: GetWorkingDir() => {0}", strWorkDir));
-            return strWorkDir;
+            return GetItemFromCache<string>(CacheElement.WorkingDir,
+                delegate()
+                {
+                    return _Git.WorkingDir;
+                });
         }
 
         public string RunGitCmd(string strCMD)
@@ -38,29 +64,35 @@ namespace MyForceReleaser
 
         public bool IsCurrentBranchMaster()
         {
-            string strCurrentBranchName = GetCurrentBranchName();
-            bool bResult = !string.IsNullOrWhiteSpace(strCurrentBranchName)
-                && System.Text.RegularExpressions.Regex.Match(strCurrentBranchName, "master", System.Text.RegularExpressions.RegexOptions.IgnoreCase).Success;
-            Logger.GetLogger().LogMessage(string.Format("GIT: IsCurrentBranchMaster() => {0}", bResult ? "true" : "false"));
-            return bResult;
+            return GetItemFromCache<bool>(CacheElement.IsMasterBranch,
+                delegate()
+                {
+                    string strCurrentBranchName = GetCurrentBranchName();
+                    return !string.IsNullOrWhiteSpace(strCurrentBranchName)
+                        && System.Text.RegularExpressions.Regex.Match(strCurrentBranchName, "master", System.Text.RegularExpressions.RegexOptions.IgnoreCase).Success;
+                });
         }
 
         public bool IsCurrentBranchFixBranch()
         {
-            string strCurrentBranchName = GetCurrentBranchName();
-            bool bResult = !string.IsNullOrWhiteSpace(strCurrentBranchName)
-                && System.Text.RegularExpressions.Regex.Match(strCurrentBranchName, @"fix-(\d+\.)+\d+", System.Text.RegularExpressions.RegexOptions.IgnoreCase).Success;
-            Logger.GetLogger().LogMessage(string.Format("GIT: IsCurrentBranchFixBranch() => {0}", bResult ? "true" : "false"));
-            return bResult;
+            return GetItemFromCache<bool>(CacheElement.IsFixBranch,
+                delegate()
+                {
+                    string strCurrentBranchName = GetCurrentBranchName();
+                    return !string.IsNullOrWhiteSpace(strCurrentBranchName)
+                        && System.Text.RegularExpressions.Regex.Match(strCurrentBranchName, @"fix-(\d+\.)+\d+", System.Text.RegularExpressions.RegexOptions.IgnoreCase).Success;
+                });
         }
 
         public bool IsCurrentBranchVersionBranch()
         {
-            string strCurrentBranchName = GetCurrentBranchName();
-            bool bResult = !string.IsNullOrWhiteSpace(strCurrentBranchName)
-                && System.Text.RegularExpressions.Regex.Match(strCurrentBranchName, @"version-(\d+\.)+\d+", System.Text.RegularExpressions.RegexOptions.IgnoreCase).Success;
-            Logger.GetLogger().LogMessage(string.Format("GIT: IsCurrentBranchVersionBranch() => {0}", bResult ? "true" : "false"));
-            return bResult;
+            return GetItemFromCache<bool>(CacheElement.IsVersionBranch,
+               delegate()
+               {
+                   string strCurrentBranchName = GetCurrentBranchName();
+                   return !string.IsNullOrWhiteSpace(strCurrentBranchName)
+                       && System.Text.RegularExpressions.Regex.Match(strCurrentBranchName, @"version-(\d+\.)+\d+", System.Text.RegularExpressions.RegexOptions.IgnoreCase).Success;
+               });
         }
     }
 }
